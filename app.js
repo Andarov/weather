2a5ee50e2b675e30756fda930468e81812a093d3
Andarov <bekturandarob678@gmail.com>
2022-11-23 22:48:02 +0500

    Add info weather all provinces of Uzbekistan

--- app.js
+++ app.js
@@ -11,7 +11,6 @@ const getData = async function(city){
 
 const getWeather = async function(city){
     const data = await getData(city);
-    console.log(data);
 
     return data
 }
@@ -19,6 +18,7 @@ const getWeather = async function(city){
 const elForm = document.querySelector('#form')
 const elCityInput = document.querySelector('#city-input')
 const elWeatherInfo = document.querySelector('#weather-info')
+const elTable = document.querySelector('#table')
 
 const update = function(weather){
     let timezone = weather.timezone;
@@ -63,6 +63,133 @@ elForm.addEventListener('submit', function(e){
 
     const cityName = elCityInput.value.trim()
     getData(cityName).then((data)=> update(data))
+    elTable.classList.add('hidden')
 
     elCityInput.value = ''
 })
+
+// Cities
+const cities = [
+    {
+        city: 'Toshkent',
+        latitude: 41.2994958,
+        longtitude: 69.24007340000003,
+    },
+    {
+        city: 'Andijon',
+        latitude: 40.815356,
+        longtitude: 72.28375,
+    },
+    {
+        city: 'Fargona',
+        latitude: 40.37338,
+        longtitude: 71.797833,
+    },
+    {
+        city: 'Namangan',
+        latitude: 41.005773,
+        longtitude: 71.643603,
+    },
+    {
+        city: 'Jizzax',
+        latitude: 40.125044,
+        longtitude: 67.880824,
+    },
+    {
+        city: 'Sirdaryo',
+        latitude: 40.837251,
+        longtitude: 68.661841,
+    },
+    {
+        city: 'Surhandaryo',
+        latitude: 37.940901,
+        longtitude: 67.570854,
+    },
+    {
+        city: 'Qashqadaryo',
+        latitude: 38.927116,
+        longtitude: 65.753931,
+    },
+    {
+        city: 'Samarqand',
+        latitude: 39.627012,
+        longtitude: 66.974973,
+    },
+    {
+        city: 'Buxoro',
+        latitude: 39.768083,
+        longtitude: 64.455577,
+    },
+    {
+        city: 'Navoiy',
+        latitude: 40.103922,
+        longtitude: 65.368834,
+    },
+    {
+        city: 'Xorazm',
+        latitude: 41.356534,
+        longtitude: 60.856669,
+    },
+    {
+        city: 'Nukus',
+        latitude: 42.461891,
+        longtitude: 59.616631,
+    },
+
+  ]
+
+  let lat;
+  let long;
+  const elTbody = document.querySelector('#tbody')
+
+  cities.forEach(function(city){
+      lat = city.latitude
+      long = city.longtitude
+
+      const getDataCity = async function(){
+        const base = `https://api.openweathermap.org/data/2.5/weather`
+        
+        const query = `?lat=${lat}&lon=${long}&units=metric&appid=${KEY}`
+    
+        const request = await fetch(base + query);
+        const data = await request.json()
+        return data
+    }
+    
+    const getWeatherCity = async function(){
+        const data = await getDataCity();
+        return data
+    }
+    
+    const dataCity = getWeatherCity()
+    dataCity.then((data)=> {
+        console.log(data);
+        elTbody.innerHTML += `
+        <tr class="flex items-center bg-white">
+            <td class="py-5 w-20 md:w-28">
+                <img class="w-20 h-20 mr-10" src="http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png" alt="temperature">
+            </td>
+            <td class="w-3/12 text-center">
+                <span class="text-sm font-normal whitespace-nowrap">${data.name}</span>
+            </td>
+            <td class="w-3/12 text-center">
+                <span class="text-sm font-normal whitespace-nowrap">${Math.floor(data.main.temp)} C</span>
+            </td>
+            <td class="w-3/12 text-center">
+                <span class="text-sm font-normal whitespace-nowrap">${data.weather[0].main}</span>
+            </td>
+            <td class="hidden w-3/12 text-center md:block">
+                <span class="text-sm font-normal">${data.wind.speed} km/h</span>
+            </td>
+            <td class="hidden w-3/12 text-center pr-6 md:block">
+                <span class="text-sm font-normal whitespace-nowrap">${data.main.humidity}%</span>
+            </td>
+        </tr>
+      `
+    });
+  })
+
+
+
+  
+
